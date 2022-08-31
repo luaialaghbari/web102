f61efc9b64b2b35b6271b4e564eafa44fe153e33
luaialaghbari <112024253+luaialaghbari@users.noreply.github.com>
2022-08-31 20:40:28 +0300

    Delete Question4.java

--- Question4.java
+++
@@ -1,7 +0,0 @@
-public class Question4 {
-    public static void main(String[] args){
-        System.out.println(1000*0.05);
-        System.out.println(1000*0.05*0.05);
-        System.out.println(1000*0.05*0.05*0.05);
-    }
-}

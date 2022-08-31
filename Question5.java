93babcad62374192569dbdc7748bd7fe3f37a46f
luaialaghbari <112024253+luaialaghbari@users.noreply.github.com>
2022-08-31 20:40:39 +0300

    Delete Question5.java

--- Question5.java
+++
@@ -1,7 +0,0 @@
-public class Question5 {
-    public static void main(String[] args){
-        System.out.println("-+-+-+");
-        System.out.println("|luai|");
-        System.out.println("+-+-+-");
-}
-}

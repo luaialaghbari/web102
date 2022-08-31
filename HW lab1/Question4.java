ba962424726e73cbbec6faa9add3b3eb75e8614b
luaialaghbari <112024253+luaialaghbari@users.noreply.github.com>
2022-08-31 20:42:14 +0300

    Add files via upload

---
+++ HW lab1/Question4.java
@@ -0,0 +1,7 @@
+public class Question4 {
+    public static void main(String[] args){
+        System.out.println(1000*0.05);
+        System.out.println(1000*0.05*0.05);
+        System.out.println(1000*0.05*0.05*0.05);
+    }
+}

014ab3d2dab4ff9f97b7dd7f059f97954143a344
luaialaghbari <112024253+luaialaghbari@users.noreply.github.com>
2022-11-07 14:42:55 +0300

    Add files via upload

---
+++ Lab6HW/src/Q4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q4 {
+
+
+        public static void main(String[] args) {
+            String word ;     int  reverse;
+            Scanner in=new Scanner(System.in);
+            System.out.println("Input your word: ");
+            word=in.next();
+            reverse=word.length();
+
+            for (int i = 1; i < word.length() ; i--) {
+                reverse--;
+                System.out.print(word.charAt(reverse));
+            }
+
+        }
+    }
+

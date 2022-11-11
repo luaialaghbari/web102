ff827c3cc180fb1e5c2696c0e1d2d2d561838739
luaialaghbari <112024253+luaialaghbari@users.noreply.github.com>
2022-11-11 18:54:35 +0300

    Update FillInQuestion.java

--- Lab7HW/Q2/FillInQuestion.java
+++ Lab7HW/Q2/FillInQuestion.java
@@ -2,15 +2,7 @@ package exercise_2;
 
 import java.util.Scanner;
 
-/**
- * Add a class FillInQuestion to the question hierarchy. Such as question is constructed with a string that contains
- * the answer, surrounded by ___.
- * 
- * 		The question should be displayed as "The inventor of Java was _____.
- * 
- * @author Mayuresh
- *
- */
+
 
 public class FillInQuestion extends Question
 {
